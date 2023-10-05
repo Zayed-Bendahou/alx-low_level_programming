9af82a2b7e3a12ea6907e5b22a7e93bb244ed3a7
Zayed-Bendahou <bendahouzayed@gmail.com>
2023-10-05 14:07:02 -0700

    m

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-
 /**
- * main - Entry point 
+ * main - Entry point
  *
- * Description: A program that prints the size of various types 
+ * Description: A program that prints the size of various types
  *
  * Return: Always 0 (Success)
 */
-
 int main(void)
 {
 
