7cbcd204ecdc02551092370f728981acf14458d4
Zayed-Bendahou <bendahouzayed@gmail.com>
2023-10-05 12:04:11 -0700

    6

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,10 +4,10 @@
  *
  * Description: a C program that prints with put function
  *
- * Reyrn: Alwats 0 (success)
- * */
+ * Reyrn: Always 0 (success)
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
