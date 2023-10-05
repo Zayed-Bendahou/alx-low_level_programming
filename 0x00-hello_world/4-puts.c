51e03461ad432437470958c1f4ab96af18590069
Zayed-Bendahou <bendahouzayed@gmail.com>
2023-10-05 12:02:13 -0700

    6

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 /**
  * main - Entry point
+ *
  * Description: a C program that prints with put function
+ *
  * Reyrn: Alwats 0 (success)
  * */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
 }
