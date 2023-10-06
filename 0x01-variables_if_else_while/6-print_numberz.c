3d63283e7c7ab9d1b87d7520add2c38cb4a1ebf7
Zayed-Bendahou <bendahouzayed@gmail.com>
2023-10-06 15:19:33 -0700

    t

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+/**
+ * main - Entry point
+ *
+ * Description: print 0-9 using int variable
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		putchar(digit + '0');
+		digit++;
+	}
+	putchar('\n');
+
+	return (0);
+}
