146a38bca6bc77583dfb6a06f0c07d4e20bb056d
Zayed-Bendahou <bendahouzayed@gmail.com>
2023-10-12 13:58:12 -0700

    0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - check if c is uppercase
+ *
+ * @c: input for letter
+ *
+ * Return: 1 if uppercase , 0 if lowercase
+*/
+
+int _isupper(int c)
+{
+	if(c >= 65 && c <=90)
+		return (1);
+	else
+		return (0);
+}
