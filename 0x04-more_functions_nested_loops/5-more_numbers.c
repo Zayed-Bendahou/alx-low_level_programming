7cd6a90c4aef3788e9e4d26a9f1459a3f129aa84
Zayed-Bendahou <bendahouzayed@gmail.com>
2023-10-12 14:51:33 -0700

    5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_more_numbers - function that prints 10 times the numbers from 0 to 14
+ *
+ * Return: Always 0 (success)
+*/
+
+void print_more_numbers(void)
+{
+	int num, row, count;
+
+	for (row = 1; row <= 10; num++)
+	{
+		for (count = 0; count <= 14; count++)
+		{
+			num = count;
+			if (count > 9)
+			{
+				_putchar(1 + 48);
+				num = count % 10;
+			}
+			_putchar(num + 48);
+		}
+		_putchar('\n');
+	}
+}
