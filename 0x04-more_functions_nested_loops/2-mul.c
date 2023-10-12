432d4232565ddfe7a922253781fb8849e417b301
Zayed-Bendahou <bendahouzayed@gmail.com>
2023-10-12 14:26:33 -0700

    3

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two ints
+ *
+ * @a: first input
+ * @b: second input
+ *
+ * Return: 1 if digit , 0 if otherwise
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+include "main.h"
+
+/**
+ * print_numbers - function that prints number from 0 to 9
+ *
+ * Return: 0 if success
+*/
+
+void print_numbers(void)
+{
+	int num = 0;
+
+	do {
+		_putchar(num + 48);
+		num++;
+	} while (num >= 0 && num <= 9);
+	_putchar('\n');
+}
