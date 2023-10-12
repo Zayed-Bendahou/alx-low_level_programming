eb56ec887d0cb4921f4583d75f34b314e77612d9
Zayed-Bendahou <bendahouzayed@gmail.com>
2023-10-12 14:29:03 -0700

    3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,9 +1,9 @@
-include "main.h"
+#include "main.h"
 
 /**
  * print_numbers - function that prints number from 0 to 9
  *
- * Return: 0 if success
+ * Return: Always 0 (success)
 */
 
 void print_numbers(void)
