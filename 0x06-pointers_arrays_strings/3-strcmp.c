3303dcdfa5bdb0f67eed2552d889228864d364df
Zayed-Bendahou <bendahouzayed@gmail.com>
2023-10-18 16:24:04 -0700

    5

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,27 @@
+#include "mian.h"
+
+/**
+ * _strcmp - compares two strings
+ *
+ * @s1: string one
+ * @s2: string two
+ *
+ * Return: 1 if true, else 0
+*/
+
+int _strmcp(char *s1, char *s2)
+{
+	int equal = 0;
+
+	while (*s1)
+	{
+		if (*s1 != *s2)
+		{
+			equal = ((int)*s1 - 48) - ((int)*s2 - 48);
+			break;
+		}
+		s1++;
+		s2++;
+	}
+	return (equal);
+}
