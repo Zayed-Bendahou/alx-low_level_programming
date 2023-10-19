2b00191245319b86eaee95800e50f2d52fb6cf0d
Zayed-Bendahou <bendahouzayed@gmail.com>
2023-10-18 18:18:31 -0700

    6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,7 +13,8 @@ char *cap_string(char *str)
 
 	while (str[index])
 	{
-		while (!(str[index] >= 'a' && str[index] <= 'z'))
+		while (!(str[index] >= 'a' && str[index]
+<= 'z'))
 			index++;
 
 		if (str[index - 1] == ' ' ||
